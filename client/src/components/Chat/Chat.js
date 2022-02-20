447f85cd57b6aae951b7b3a376cffe40136ca2fd
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2022-02-20 12:48:44 +0600

    Successfully Connected Socket.io

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 import './Chat.css';
 
+let socket;
+
 const Chat = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+    const ENDPOINT = 'http://localhost:5000/';
+
+    const location = useLocation();
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+
+        setName(name);
+        setRoom(room);
+
+        socket = io(ENDPOINT);
+        console.log(socket);
+        
+    });
     return (
         <div>
             <h1>Chat</h1>
--- server/index.js
+++ server/index.js
@@ -8,7 +8,12 @@ const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"]
+    }
+  });
 
 io.on('connection', (socket) => {
     console.log('New connection!');
